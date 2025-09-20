7ebb316c6291802285482633af77ae3c08f014ba
Borislav-Madzharov <borislavmadzharov10@gmail.com>
2025-09-20 18:22:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.PadawanEquipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            double students = double.Parse(Console.ReadLine());
+            double lightsaberPrice = double.Parse(Console.ReadLine());
+            double robesPrice = double.Parse(Console.ReadLine());
+            double beltsPrice = double.Parse(Console.ReadLine());
+
+            double freeBelts = 0.16 * lightsaberPrice * students;
+
+            double totalPrice = ((Math.Ceiling(1.10 * students) * lightsaberPrice) + (students * robesPrice) +  (students * beltsPrice + freeBelts));
+
+            Console.WriteLine();
+        }
+    }
+}
